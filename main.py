435f14984438535b61544b0cc889780b40755e3d
iboutadg <iboutadg@student.1337.ma>
2024-02-02 22:31:13 +0100

    can convert video

--- main.py
+++ main.py
@@ -3,13 +3,16 @@
 import cv2 as cv
 import time
 import os
+import sys
 
 os.system("make")
-cam = cv.VideoCapture(0)
+if (len(sys.argv) < 2):
+    cam = cv.VideoCapture(0)
+else:
+    cam = cv.VideoCapture(sys.argv[1])
 while(True):
     result, img = cam.read()
     if (result):
         img = cv.flip(img, 1)
         cv.imwrite("test.png",img)
-        #os.system("clear")
         os.system("./img_to_ascii test.png")
